86ea56bb73fdaece09612469268aeffecadf503d
cyvolta <cyvolta@gmail.com>
2024-10-17 12:49:58 +0300

    feat: Font Loading
    - Loaded the font in the public folder
    
    - Created created a proxy in lib to load fonts
    
    - Loaded the font

---
+++ app/lib/fonts.ts
@@ -0,0 +1,96 @@
+import localFont from 'next/font/local';
+
+export const workSans = localFont({
+    src: [
+        {
+            path: '../../public/fonts/work_sans/regular/100.ttf',
+            weight: '100',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/work_sans/regular/200.ttf',
+            weight: '200',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/work_sans/regular/300.ttf',
+            weight: '300',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/work_sans/regular/400.ttf',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/work_sans/regular/500.ttf',
+            weight: '500',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/work_sans/regular/600.ttf',
+            weight: '600',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/work_sans/regular/700.ttf',
+            weight: '700',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/work_sans/regular/800.ttf',
+            weight: '800',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/work_sans/regular/900.ttf',
+            weight: '900',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/work_sans/italic/100.ttf',
+            weight: '100',
+            style: 'italic',
+        },
+        {
+            path: '../../public/fonts/work_sans/italic/200.ttf',
+            weight: '200',
+            style: 'italic',
+        },
+        {
+            path: '../../public/fonts/work_sans/italic/300.ttf',
+            weight: '300',
+            style: 'italic',
+        },
+        {
+            path: '../../public/fonts/work_sans/italic/400.ttf',
+            weight: '400',
+            style: 'italic',
+        },
+        {
+            path: '../../public/fonts/work_sans/italic/500.ttf',
+            weight: '500',
+            style: 'italic',
+        },
+        {
+            path: '../../public/fonts/work_sans/italic/600.ttf',
+            weight: '600',
+            style: 'italic',
+        },
+        {
+            path: '../../public/fonts/work_sans/italic/700.ttf',
+            weight: '700',
+            style: 'italic',
+        },
+        {
+            path: '../../public/fonts/work_sans/italic/800.ttf',
+            weight: '800',
+            style: 'italic',
+        },
+        {
+            path: '../../public/fonts/work_sans/italic/900.ttf',
+            weight: '900',
+            style: 'italic',
+        },
+    ],
+});
