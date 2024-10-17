fba91cea842858aecd7fe2cb155aec430b3fcf31
cyvolta <cyvolta@gmail.com>
2024-10-17 13:10:07 +0300

    feat: Metadata - updated Landing page metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Archetype',
+};
+
+export default function Landing() {
     return <></>;
 }
